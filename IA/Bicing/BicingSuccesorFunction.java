2a7c8841f2e8e000e1e995d4b632271657cc917a
Yiqi Zheng <yiqiyt@gmail.com>
2023-10-14 14:10:17 +0200

    push
    Co-authored-by: Jianing <jianingxu1@users.noreply.github.com>

--- IA/Bicing/BicingSuccesorFunction.java
+++ IA/Bicing/BicingSuccesorFunction.java
@@ -10,25 +10,25 @@ public class BicingSuccesorFunction implements SuccessorFunction {
     public List getSuccessors(Object state) {
         ArrayList retval = new ArrayList();
         State board = (State) state;
-        int F = State.getF();
-        int E = State.getE();
+        // int F = State.getF();
+        // int E = State.getE();
 
-        for (int vanId = 0; vanId < F; ++vanId) {
-            for (int originId = 0; originId < E; ++originId) {
-                // van can go to same originId OR not visited station
+        // for (int vanId = 0; vanId < F; ++vanId) {
+        //     for (int originId = 0; originId < E; ++originId) {
+        //         // van can go to same originId OR not visited station
 
-                for (int destId1 = 0; destId1 < E; ++destId1) {
-                    if (destId1 == originId)
-                        continue;
-                    // single move
-                    for (int destId2 = 0; destId2 < E; ++destId2) {
-                        if (destId2 == originId || destId2 == destId1)
-                            continue;
-                        // double move
-                    }
-                }
-            }
-        }
+        //         for (int destId1 = 0; destId1 < E; ++destId1) {
+        //             if (destId1 == originId)
+        //                 continue;
+        //             // single move
+        //             for (int destId2 = 0; destId2 < E; ++destId2) {
+        //                 if (destId2 == originId || destId2 == destId1)
+        //                     continue;
+        //                 // double move
+        //             }
+        //         }
+        //     }
+        // }
         return (retval);
     }
 }
--- IA/Bicing/State.java
+++ IA/Bicing/State.java
@@ -1,5 +1,6 @@
 //BicingBoard.java
 package IA.Bicing;
+import java.util.ArrayList;
 
 /**
  * Created by bejar on 17/01/17.
@@ -8,11 +9,12 @@ public class State {
     // originId, bikesTaken, destId1, numBikesLeftDest2, destId2,
     // numBikesLeftDest2
     private int[][] fleet; // .size .insert
-    private Boolean[] isStationVisited;
-    private int[] demandMinusSupply; // preguntarle al profe
+    private int[] isStationVisited; // if not visited, -1
+                                    // else, contains the id of the van that visits it
+    private int[] bikesNeeded;
 
     private double transportCost;
-    private double benefit; // benefit == demandSupplied - cost;
+    private double benefit; // benefit = suppliedDemand - transportCost;
     private int suppliedDemand; //
 
     private static Estaciones stations;
@@ -21,64 +23,52 @@ public class State {
 
     /* Constructor */
 
-    //Hay que separar positivos con negativos con 2 vectores static
-    private static Estaciones estacionesPositivas;
-    private static Estaciones estacionesNegativas;
-
-
     public State(int n_van, Estaciones Est) {
+        State.stations = Est;
+        State.F = n_van;
+        State.E = Est.size();
+
+        this.fleet = new int[n_van][6];
+        this.isStationVisited = new int[Est.size()];
+
+        for (int i = 0; i < State.E; ++i) {
+            this.isStationVisited[i] = -1;
+        }
+        
         initialize_supply(Est);
 
         this.transportCost = 0;
         this.benefit = 0;
         this.suppliedDemand = 0;
+        
 
-        State.stations = Est;
-        State.F = n_van;
-        State.E = Est.size();
-        
-        for (int i = 0; i < E; ++i) {
-            if (stations.get(i).getNumBicicletasNext() - stations.get(i).getDemanda() > 0) {
-                estacionesPositivas.add(stations.get(i));
-            }
-            else if (stations.get(i).getNumBicicletasNext() - stations.get(i).getDemanda() < 0) {
-                estacionesNegativas.add(stations.get(i));
-            }
-        }
-        
-        this.isStationVisited = new Boolean[estacionesPositivas.size()];
-        
-        sort_positivo();
-        sort_negativo();
     }
 
     // Copy constructor
     public State(final State state) {
         this.setFleet(state.getFleet());
         this.setIsStationVisited(state.getIsStationVisited());
-        this.setDemandMinusSupply(state.getDemandMinusSupply());
+        this.setBikesNeeded(state.getBikesNeeded());
 
         this.setTransportCost(state.getTransportCost());
         this.setBenefit(state.getBenefit());
         this.setSuppliedDemand(state.getSuppliedDemand());
 
         this.setStations(getStations());
-        this.setDemandMinusSupply(state.getDemandMinusSupply());
+        this.setBikesNeeded(state.getBikesNeeded());
         this.setCost(state.getCost());
-        this.setBenefits(state.getBenefits());
         this.setDemandSupplied(state.getDemandSupplied());
     }
 
     /* Operators */
-    
-    public void changeDestination1(int fleetId, int newDestId) {
-        int originId = fleet[fleetId][0];
-        int bikesTaken = fleet[fleetId][1];
-        int dest1Id = fleet[fleetId][2];
-        int numBikesLeftDest1 = fleet[fleetId][3];
-        int dest2Id = fleet[fleetId][4];
-        int numBikesLeftDest2 = fleet[fleetId][5];
-        Boolean newDestNeedsBikes = demandMinusSupply[newDestId] < 0;
+    public void changeDestination1(int vanId, int newDestId) {
+        int originId = fleet[vanId][0];
+        int bikesTaken = fleet[vanId][1];
+        int dest1Id = fleet[vanId][2];
+        int numBikesLeftDest1 = fleet[vanId][3];
+        int dest2Id = fleet[vanId][4];
+        int numBikesLeftDest2 = fleet[vanId][5];
+        Boolean newDestNeedsBikes = bikesNeeded[newDestId] < 0;
 
 
         if (newDestId == originId || !newDestNeedsBikes) return;
@@ -86,7 +76,38 @@ public class State {
         // second destination
         if (newDestId == -1 && dest2Id != -1) return;
 
-        // int previousCost = transportCost
+        // Update transport cost from origin to dest1 (bikesTaken is the same, bc we come from origin)
+        // Calculate the transport cost of the previous route
+        double previousTransportCost1 = getVanTransportCost(originId, dest1Id, bikesTaken);
+        double previousTransportCost2 = getVanTransportCost(dest1Id, dest2Id, bikesTaken - numBikesLeftDest1);
+        // 
+        
+        this.suppliedDemand -= numBikesLeftDest1 + numBikesLeftDest2;
+
+        fleet[vanId][3] = numBikesLeftDest1 = Math.min(bikesTaken, bikesNeeded[newDestId]);
+        fleet[vanId][5] = numBikesLeftDest2 = Math.min(bikesTaken - numBikesLeftDest1, bikesNeeded[dest2Id]);
+        dest1Id = fleet[vanId][2] = newDestId;
+
+        
+        double newTransportCost1 = getVanTransportCost(originId, newDestId, bikesTaken);
+        double newTransportCost2 = getVanTransportCost(newDestId, dest2Id, bikesTaken - numBikesLeftDest1);
+
+        this.transportCost = transportCost - (previousTransportCost1 + previousTransportCost2) + (newTransportCost1 + newTransportCost2);
+
+        this.suppliedDemand += numBikesLeftDest1 + numBikesLeftDest2;
+
+        // Update transport cost from dest1 to dest2
+
+
+        // Get previous and new supplied
+        int previousBikesSupplied = numBikesLeftDest1;  // Supposing that all the bikes we leave, gives us benefit
+        int newBikesSupplied = Math.min(bikesTaken, bikesNeeded[dest1Id]);
+        
+        // Calculate new benefit
+        this.benefit = (float) suppliedDemand - transportCost;
+
+        // Update van
+        
     }
 
     public void changeDestination2(int fleetId, int newDestId) {
@@ -96,13 +117,11 @@ public class State {
         int numBikesLeftDest1 = fleet[fleetId][3];
         int dest2Id = fleet[fleetId][4];
         int numBikesLeftDest2 = fleet[fleetId][5];
-        Boolean newDestNeedsBikes = demandMinusSupply[newDestId] < 0;
+        Boolean newDestNeedsBikes = bikesNeeded[newDestId] < 0;
 
 
         if (newDestId == originId || !newDestNeedsBikes) return;
         if (newDestId == dest1Id) return;
-        
-
     }
 
     public void swapOrigin(int fleetId, int newOriginId) {
@@ -129,12 +148,12 @@ public class State {
         return this.fleet;
     }
 
-    public Boolean[] getIsStationVisited() {
+    public int [] getIsStationVisited() {
         return this.isStationVisited;
     }
 
-    public int[] getDemandMinusSupply() {
-        return this.demandMinusSupply;
+    public int[] getBikesNeeded() {
+        return this.bikesNeeded;
     }
 
     public double getTransportCost() {
@@ -155,10 +174,6 @@ public class State {
 
     public double getCost() {
         return this.transportCost;
-    }
-
-    public double getBenefits() {
-        return this.benefit;
     }
 
     public int getDemandSupplied() {
@@ -185,19 +200,19 @@ public class State {
         }
     }
 
-    public void setIsStationVisited(final Boolean[] isStationVisited) {
+    public void setIsStationVisited(final int[] isStationVisited) {
         int E = isStationVisited.length;
-        this.isStationVisited = new Boolean[E];
+        this.isStationVisited = new int[E];
         for (int i = 0; i < E; ++i) {
             this.isStationVisited[i] = isStationVisited[i];
         }
     }
 
-    public void setDemandMinusSupply(final int[] demandMinusSupply) {
-        int E = demandMinusSupply.length;
-        this.demandMinusSupply = new int[E];
+    public void setBikesNeeded(final int[] bikesNeeded) {
+        int E = bikesNeeded.length;
+        this.bikesNeeded = new int[E];
         for (int i = 0; i < E; ++i) {
-            this.demandMinusSupply[i] = demandMinusSupply[i];
+            this.bikesNeeded[i] = bikesNeeded[i];
         }
     }
 
@@ -232,14 +247,16 @@ public class State {
     /* Initializers */
 
     private void initialize_supply(Estaciones Est) {
-        this.demandMinusSupply = new int[Est.size()];
+        this.bikesNeeded = new int[Est.size()];
         for (int i = 0; i < Est.size(); i++) {
-            demandMinusSupply[i] = Est.get(i).getDemanda() - Est.get(i).getNumBicicletasNext();
+            bikesNeeded[i] = (Est.get(i).getDemanda() - Est.get(i).getNumBicicletasNext());
+            if (bikesNeeded[i] > 0) bikesNeeded[i] = bikesNeeded[i]%30;
         }
     }
 
     public void initialize_easy() {
         fleet = new int[F][6];
+        // Estrategia: 
         for (int i = 0; i < F; i++) {
             for (int j = 0; j < 6; j++) {
                 this.fleet[i][j] = -1;
@@ -248,71 +265,108 @@ public class State {
     }
 
     public void initialize_medium() {
-        int size_pos = estacionesPositivas.size();
-        int size_neg = estacionesNegativas.size();
         int size_fleet = F;
         int i = 0;
-
-        while (size_pos > i && size_neg > i && size_fleet > 0) {
-            fleet[i][0] = i;
-            fleet[i][1] = fleet[i][3] = (estacionesPositivas.get(i).getDemanda() - estacionesPositivas.get(i).getNumBicicletasNext())%30;
-            fleet[i][2] = i;
-            fleet[i][4] = fleet[i][5] = -1;
-
-            isStationVisited[i] = true;
-
-            //set numBicicletasNoUsadas
-            estacionesPositivas.get(i).setNumBicicletasNoUsadas(estacionesPositivas.get(i).getDemanda() - estacionesPositivas.get(i).getNumBicicletasNext() - fleet[i][1]);
-            int not_used_bikes = estacionesNegativas.get(i).getDemanda() - estacionesNegativas.get(i).getNumBicicletasNext() + fleet[i][1];
-            if (not_used_bikes < 0) estacionesNegativas.get(i).setNumBicicletasNoUsadas(0);
-            else estacionesNegativas.get(i).setNumBicicletasNoUsadas(not_used_bikes);
-
-            //set transportCost
-            transportCost += calcula_cost(i, i, fleet[i][1]);
+        int j = 0;
+        
+        while (i < E && j < E && size_fleet > 0) {
+            i = 0;
+            j = 0;
+            //print bikes Needed
+            for (int k = 0; k < E; ++k) {
+                System.out.print(bikesNeeded[k] + " ");
+            }
+            //llegas al primer positivo
+            while (bikesNeeded[i] <= 0 && i < E) {
+                ++i;
+            }
+            //llegas al primer negativo
+            while (bikesNeeded[j] >= 0 && j < E) {
+                ++j;
+            }
             
-            //set numBicicletasNext
-            estacionesPositivas.get(i).setNumBicicletasNext(estacionesPositivas.get(i).getDemanda() - fleet[i][1]);
-            estacionesNegativas.get(i).setNumBicicletasNext(estacionesNegativas.get(i).getDemanda() + fleet[i][1]);
-            --size_fleet;
-            --size_neg;
-            --size_pos;
-            ++i;
+            System.out.println("i: " + i + " j: " + j + " size_fleet: " + size_fleet);
+            
+            /*-15 -1 2 -26 -22 -10 -12 -8 3 19 10 -2 -10 22 -17 4 -23 4 9 0 28 -22 -16 27 -9 i: 2 j: 0 size_fleet: 5
+              -13 -1 0 -26 -22 -10 -12 -8 3 19 10 -2 -10 22 -17 4 -23 4 9 0 28 -22 -16 27 -9 i: 8 j: 0 size_fleet: 4
+              -10 -1 0 -26 -22 -10 -12 -8 0 19 10 -2 -10
+              0 -1 0 -26 -22 -10 -12 -8 0 9 10 -2 -10
+              0 0 0 -26 -22 -10 -12 -8 0 0 10 -2 -10
+              0 0 0 -16 -22 -10 -12 -8 0 0 0 -2 -10*/
+
+            //se usa de origen?
+            if (isStationVisited[i] == -1) {
+                int k = F - size_fleet;
+                isStationVisited[i] = k;
+                
+                fleet[k][0] = i; //origen
+                int temp1 = bikesNeeded[i];
+                int temp2 = Math.abs(bikesNeeded[j]);
+                if (temp1 > temp2) {
+                    fleet[k][1] = temp2; //bicis que sale de origen
+                    fleet[k][3] = temp2; //bicis que llegan a destino
+                    bikesNeeded[i] += temp2;
+                    bikesNeeded[j] -= temp2;
+                }
+                else {
+                    fleet[k][1] = temp1; //bicis que sale de origen
+                    fleet[k][3] = temp1; //bicis que llegan a destino
+                    bikesNeeded[i] = bikesNeeded[i] - temp1;
+                    bikesNeeded[j] += temp1;
+                }
+
+                fleet[k][2] = j; //destino
+                fleet[k][4] = -1;
+                fleet[k][5] = -1; //no hay segundo destino
+
+                //set suppliedDemand
+                suppliedDemand += fleet[k][3];
+                //update size_fleet
+                --size_fleet;
+            }
+            else {
+                int h = isStationVisited[i];
+                //tiene 2o destino?
+                if (fleet[h][4] == -1) {
+                    fleet[h][4] = j;
+                    int temp1 = bikesNeeded[i];
+                    int temp2 = Math.abs(bikesNeeded[j]);
+                    if (temp1 > temp2) {
+                        fleet[h][1] += temp2; //bicis que sale de origen
+                        fleet[h][5] = temp2; //bicis que sale de origen
+                        bikesNeeded[i] = 0; //ponemos a cero porque ya ha hecho 2 destinos y aun le sobran
+                        bikesNeeded[j] -= temp2;
+                    }
+                    else {
+                        fleet[h][1] += temp1; //bicis que sale de origen
+                        fleet[h][5] = temp1; //bicis que sale de origen
+                        bikesNeeded[i] -= temp1;
+                        bikesNeeded[j] += temp1;
+                    }
+                    //set suppliedDemand
+                    suppliedDemand += Math.abs(fleet[h][5]);
+                }
+            }
         }
+        //set transportCost
+        int l = 0;
+        while (l < F && fleet[l][1] != -1) {
+            transportCost += calcula_cost(fleet[l][0], fleet[l][2], fleet[l][1]);
+            if (fleet[l][4] != -1)
+                transportCost += calcula_cost(fleet[l][2], fleet[l][4], fleet[l][3]);
+            ++l;
+        }
+        //set benefit
+        benefit = suppliedDemand - transportCost;
+
+        System.out.println("benefit: " + benefit + " transportCost: " + transportCost + " suppliedDemand: " + suppliedDemand);
     }
 
     public double calcula_cost(int i, int j, int num_bicis) {
         int cost_km = (num_bicis+9)/10;
-        int distance = getEuclideanDistance(estacionesPositivas.get(i).getCoordX(), estacionesPositivas.get(i).getCoordY()estacionesNegativas.get(i).getCoordX(), estacionesNegativas.get(i).getCoordY());
-        distance /= 1000;
+        int distance = getEuclideanDistance(stations.get(i).getCoordX(), stations.get(i).getCoordY(), stations.get(j).getCoordX(), stations.get(j).getCoordY());
+        distance = distance/1000;
         return cost_km*distance;
-    }
-
-    /* Sorting functions */
-
-    //sort de mayor a menor la clase estacionesPositivas
-    private void sort_positivo() {
-        for (int i = 0; i < estacionesPositivas.size(); ++i) {
-            for (int j = i + 1; j < estacionesPositivas.size(); ++j) {
-                if (estacionesPositivas.get(i).getNumBicicletasNext() < estacionesPositivas.get(j).getNumBicicletasNext()) {
-                    Estacion aux = estacionesPositivas.get(i);
-                    estacionesPositivas.set(i, estacionesPositivas.get(j));
-                    estacionesPositivas.set(j, aux);
-                }
-            }
-        }
-    }
-
-    //sort de menor a mayor la clase estacionesNegativas
-    private void sort_negativo() {
-        for (int i = 0; i < estacionesNegativas.size(); ++i) {
-            for (int j = i + 1; j < estacionesNegativas.size(); ++j) {
-                if (estacionesNegativas.get(i).getNumBicicletasNext() > estacionesNegativas.get(j).getNumBicicletasNext()) {
-                    Estacion aux = estacionesNegativas.get(i);
-                    estacionesNegativas.set(i, estacionesNegativas.get(j));
-                    estacionesNegativas.set(j, aux);
-                }
-            }
-        }
     }
 
     // Auxiliary functions
@@ -320,7 +374,12 @@ public class State {
         return Math.abs(destX - originX) + Math.abs(destY - originY);
     }
 
-    private double getVanTransportCost() {
-        int dist
-nce = getEuclideanDistance    }
+    private double getVanTransportCost(int originId, int destId, int bikesTaken) {
+        Estacion origin = stations.get(originId);
+        Estacion dest = stations.get(destId);
+        int distance = getEuclideanDistance(origin.getCoordX(), origin.getCoordY(), dest.getCoordX(), dest.getCoordY());
+        int cost_km = (bikesTaken+9)/10;
+        distance /= 1000;
+        return cost_km*distance;
+    }
 }
